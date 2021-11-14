1439df321c4ebe4a7d560ffad735eb138691d73f
DESKTOP-OP0FSSP\anast <anastasiu.tudor@gmail.com>
2021-11-15 00:06:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [change,setChange] = useState(false);
+ 
+  const triggerToggle = () => {
+    setChange(!change);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${change ? 'dark-bg' : 'light-bg'}`} >
+      <h1 className={`title ${change ? 'light-title': 'dark-title'}`}>Dark Mode App</h1>
+      <button className={`btn ${change ? 'dark-btn': 'light-btn'}`} onClick={triggerToggle}>Toggle Mode</button>
+
+      <article>
+        <div className={`title-article ${change ? 'title-dark': 'title-light'}`} >The 5 Stages of the Creative Process</div>
+        <small className={`release-date ${change ? 'text-light' : 'text-dark'}`}>Saturday 11th, 2018  5 min read</small>
+        <p className={`text ${change ? 'text-light' : 'text-dark'}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </article>
+      <article>
+        <div className={`title-article ${change ? 'title-dark': 'title-light'}`}>A Problem in Need of a Creative Solution</div>
+        <small className={`release-date ${change ? 'text-light' : 'text-dark'}`}>Monday 16th, 2014 10 min read</small>
+        <p className={`text ${change ? 'text-light' : 'text-dark'}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      </article>
+      <article>
+        <div className={`title-article ${change ? 'title-dark': 'title-light'}`}>The Idea in Practice</div>
+        <small className={`release-date ${change ? 'text-light' : 'text-dark'}`}>Wednesday 24th, 2020 20 min read</small>
+        <p className={`text ${change ? 'text-light' : 'text-dark'}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </article>
     </div>
   );
 }
